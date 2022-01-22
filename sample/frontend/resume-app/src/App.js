a38367ad1dbdca2ad1b8be2d6f1589bd2431fafa
Tyler Youngs <tyoungs@umich.edu>
2022-01-22 18:22:54 -0500

    different landing page

--- sample/frontend/resume-app/src/App.js
+++ sample/frontend/resume-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, you're looking at a Raspberry Pi served webpage
         </p>
         <a
           className="App-link"
