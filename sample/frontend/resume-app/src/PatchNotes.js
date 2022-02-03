4ad2cd5e7011cf98b36a582b7b4cf099ccc28fc6
Tyler Youngs <tyoungs@umich.edu>
2022-02-02 21:37:20 -0500

    Placeholder static content in, pre dynamic json content

--- sample/frontend/resume-app/src/PatchNotes.js
+++ sample/frontend/resume-app/src/PatchNotes.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class PatchNotes extends Component {
     render() {
         return (
-            <div>
+            <div className='Patch-notes'>
                 <h1>Patch Notes</h1>
                 <p>Patch Note 1 - Baseline home landing pages, a resume page for Frank and I, 
                     and some project info pages have been created. Yarn has been implemented as the package
--- sample/frontend/resume-app/src/TylerResume.js
+++ sample/frontend/resume-app/src/TylerResume.js
@@ -7,27 +7,47 @@ export default class TylerResume extends Component {
         return (
             <div className="Resume">
                 <h1>Tyler</h1>
-                <p>Coding Languages</p>
-                <Accordion defaultActiveKey="0">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>C++</Accordion.Header>
-                        <Accordion.Body>
-                            I took all my undergrad classes in python
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>Python</Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                            est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+                <p>Experience</p>
+                <div className='Languages-container'>
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>C++</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                I took all my undergrad classes in C++
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Python</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                The majority of my work post graduation has been in python
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Google Cloud Platform</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                Bigquery, cloud functions, Pub/Sub, Monitoring
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Azure</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                Azure Data Factory, Azure Functions, Kubernetes, Networking
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header>Databricks</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                FHIR Processing
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header>Other</Accordion.Header>
+                            <Accordion.Body className='Languages'>
+                                ?
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
             </div>
         )
     }
